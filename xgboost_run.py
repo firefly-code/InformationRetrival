9149493845a5efb28f4d92897448bc542bae4f29
firefly-code <paul.jose.zimmer@gmail.com>
2025-11-05 15:30:34 +0100

    baseline

--- xgboost_run.py
+++ xgboost_run.py
@@ -1,51 +1,77 @@
-import pyterrier as pt 
-import xgboost as xgb
+import os
+import pyterrier as pt
 import pandas as pd
+import xgboost as xgb
+import re
 
-
+from sklearn.feature_extraction.text import TfidfVectorizer,  ENGLISH_STOP_WORDS
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 pt.init()
+ds = pt.get_dataset("irds:disks45/nocr/trec-robust-2004")
 
-#need saved indexing method
-indexing = pt.IndexRef.of("rev_indexing_path")
-#pipeline = pt.apply.doc_features()
+INDEX_DIR = r"C:\Users\paulj\OneDrive\Desktop\InfoRetrival\InformationRetrival\index"
+os.makedirs(INDEX_DIR, exist_ok=True)
 
-
-#need access to data
-train_topics, train_qrels, validation_topics, validation_qrels = None
+index_ref = pt.IndexRef.of(INDEX_DIR)
 
 
-#example feature
-numf = 2
-bm25 = pt.BatchRetrieve(indexing, wmodel="BM25")
-pl2 = pt.BatchRetrieve(indexing, wmodel="PL2")
+topics = ds.get_topics().rename(columns={"title": "query"})
+topics["query"] = topics["query"].apply(lambda x: re.sub(r'[^A-Za-z0-9 ]+', ' ', x))
+qrels = ds.get_qrels()
 
-pipeline = bm25
-pipeline = pipeline >> pl2
+split = int(len(topics) * 0.8)
+train_topics = topics.iloc[:split]
+valid_topics = topics.iloc[split:]
+train_qrels = qrels[qrels["qid"].isin(train_topics["qid"])]
+valid_qrels = qrels[qrels["qid"].isin(valid_topics["qid"])]
 
-xg_model = xgb.XGBRanker(objective = "rank:MAP",
-    learning_rate = 0.1,
+# pipeline = pt.terrier.FeaturesRetriever(
+#     index_ref,
+#     wmodel="BM25",
+#     features=["WMODEL:BM25"]
+# )
+
+bm25 = pt.terrier.Retriever(index_ref, wmodel="BM25")
+tf = pt.terrier.Retriever(index_ref, wmodel="Tf")
+pl2 = pt.terrier.Retriever(index_ref, wmodel="PL2")
+
+pipeline = bm25 >> (tf ** pl2)
+
+
+xgb_ranker = xgb.XGBRanker(
+    objective="rank:ndcg",
+    learning_rate=0.1,
     gamma=1.0,
     min_child_weight=0.1,
     max_depth=6,
-    verbose=2,
-    random_state=42 
-)
-rankers = []
-xg_model_pipe = pipeline >> pt.ltr.apply_learned_model(xg_model, form="ltr")
-xg_model_pipe.fit(train_topics, train_qrels, validation_topics, validation_qrels)
-rankers.append(xg_model_pipe)
-
-# evaluate the full (4 features) model, as well as the each model containing only 3 features)
-eval= pt.Experiment(
-    rankers,
-    validation_topics,
-    validation_qrels,
-    metrics=["map","recip_rank","P_5","R_5"],
-    names=["Full Model"]  + ["Full Minus %d" % fid for fid in range(numf)]
+    random_state=42
 )
 
-eval_results = eval.run()
+# def keyword_overlap(row):
+#     print(row)
+#     query_terms = set(re.findall(r'\w+', row['query'].lower()))
+#     doc_terms = set(re.findall(r'\w+', row['text'].lower()))
+#     query_terms -= set(ENGLISH_STOP_WORDS)
+#     doc_terms -= set(ENGLISH_STOP_WORDS)
+#     return len(query_terms.intersection(doc_terms))
 
-print(pd.DataFrame(eval_results))
+# overlap_query_title = pt.apply.doc_score(keyword_overlap)
+
+
+ltr_pipeline = pipeline >> pt.ltr.apply_learned_model(xgb_ranker, form="ltr")
+
+ltr_pipeline.fit(train_topics, train_qrels, valid_topics, valid_qrels)
+results = ltr_pipeline.transform(valid_topics)
+results.to_csv("ltr_results.csv", index=False)
+
+
+print(results.head(10))
+
+#trained_model = ltr_pipeline[-1].learner
+
+
+evalution = pt.Experiment([ltr_pipeline],valid_topics,valid_qrels, eval_metrics=["map", "ndcg_cut_10"])
+print(pd.DataFrame(evalution))
+
